0439507fc3c5527c24d8d5c1052c2a9da3bf0818
JamesLimos <limosjamesfrancis@gmail.com>
2025-01-15 20:50:07 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        // main level
+        static void Main(string[] args)
+        {
+            //promp user for input
+            Console.WriteLine("James limos");
+            string username = Console.ReadLine(); // gets the user input and saves it to 
+
+            Console.WriteLine("Hi" + username + ". My name is James");
+            Console.WriteLine("")
+        }
+    }
+}
